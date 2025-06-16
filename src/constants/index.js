0a4f899cf0c6adcfdb70e91c1f3898732eccf3d1
Amanjeet Pani <amanjeetpani2405@gmail.com>
2025-06-16 05:53:43 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -37,53 +37,53 @@ const counterItems = [
 
 const logoIconsList = [
   {
-    imgPath: "/images/logos/company-logo-1.png",
+    imgPath: "./images/logos/company-logo-1.png",
   },
   {
-    imgPath: "/images/logos/company-logo-2.png",
+    imgPath: "./images/logos/company-logo-2.png",
   },
   {
-    imgPath: "/images/logos/company-logo-3.png",
+    imgPath: "./images/logos/company-logo-3.png",
   },
   {
-    imgPath: "/images/logos/company-logo-4.png",
+    imgPath: "./images/logos/company-logo-4.png",
   },
   {
-    imgPath: "/images/logos/company-logo-5.png",
+    imgPath: "./images/logos/company-logo-5.png",
   },
   {
-    imgPath: "/images/logos/company-logo-6.png",
+    imgPath: "./images/logos/company-logo-6.png",
   },
   {
-    imgPath: "/images/logos/company-logo-7.png",
+    imgPath: "./images/logos/company-logo-7.png",
   },
   {
-    imgPath: "/images/logos/company-logo-8.png",
+    imgPath: "./images/logos/company-logo-8.png",
   },
   {
-    imgPath: "/images/logos/company-logo-9.png",
+    imgPath: "./images/logos/company-logo-9.png",
   },
   {
-    imgPath: "/images/logos/company-logo-10.png",
+    imgPath: "./images/logos/company-logo-10.png",
   },
   {
-    imgPath: "/images/logos/company-logo-11.png",
+    imgPath: "./images/logos/company-logo-11.png",
   },
 ];
 
 const abilities = [
   {
-    imgPath: "/images/seo.png",
+    imgPath: "./images/seo.png",
     title: "Quality Focus",
     desc: "Delivering high-quality results while maintaining attention to every detail.",
   },
   {
-    imgPath: "/images/chat.png",
+    imgPath: "./images/chat.png",
     title: "Reliable Communication",
     desc: "Keeping you updated at every step to ensure transparency and clarity.",
   },
   {
-    imgPath: "/images/time.png",
+    imgPath: "./images/time.png",
     title: "On-Time Delivery",
     desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
   },
@@ -92,54 +92,54 @@ const abilities = [
 const techStackImgs = [
   {
     name: "Frontend Developer",
-    imgPath: "/images/logos/react.png",
+    imgPath: "./images/logos/react.png",
   },
   {
     name: "Python Developer",
-    imgPath: "/images/logos/python.svg",
+    imgPath: "./images/logos/python.svg",
   },
   {
     name: "Backend Developer",
-    imgPath: "/images/logos/node.png",
+    imgPath: "./images/logos/node.png",
   },
   {
     name: "C++ Developer",
-    imgPath: "/images/logos/three.png",
+    imgPath: "./images/logos/three.png",
   },
   {
     name: "Project Manager",
-    imgPath: "/images/logos/git.svg",
+    imgPath: "./images/logos/git.svg",
   },
 ];
 
 const techStackIcons = [
   {
     name: "Frontend Developer",
-    modelPath: "/models/react_logo-transformed.glb",
+    modelPath: "./models/react_logo-transformed.glb",
     scale: 1,
     rotation: [0, 0, 0],
   },
   {
     name: "Backend Developer",
-    modelPath: "/models/node-transformed.glb",
+    modelPath: "./models/node-transformed.glb",
     scale: 5,
     rotation: [0, -Math.PI / 2, 0],
   },
   {
     name: "C++ Developer",
-    modelPath: "/models/three.js-transformed.glb",
+    modelPath: "./models/three.js-transformed.glb",
     scale: 0.05,
     rotation: [0, 0, 0],
   },
   {
     name: "Python Developer",
-    modelPath: "/models/python-transformed.glb",
+    modelPath: "./models/python-transformed.glb",
     scale: 0.8,
     rotation: [0, 0, 0],
   },
   {
     name: "Project Manager",
-    modelPath: "/models/git-svg-transformed.glb",
+    modelPath: "./models/git-svg-transformed.glb",
     scale: 0.05,
     rotation: [0, -Math.PI / 4, 0],
   },
@@ -148,8 +148,8 @@ const techStackIcons = [
 const expCards = [
   {
     review: "Aman’s work at Synopsys brought a high level of quality and efficiency. He delivered solutions that enhanced our customer experience & meet our product goals.",
-    imgPath: "/images/exp1.png",
-    logoPath: "/images/logo1.png",
+    imgPath: "./images/exp1.png",
+    logoPath: "./images/logo1.png",
     title: "Software Developer @ Synopsys",
     date: "January 2023 - Present",
     responsibilities: [
@@ -160,8 +160,8 @@ const expCards = [
   },
   {
     review: "Aman’s work at Mathworks brought a high level of quality and efficiency. He delivered solutions that enhanced our customer experience & meet our product goals.",
-    imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
+    imgPath: "./images/exp2.png",
+    logoPath: "./images/logo2.png",
     title: "Software Developer Intern @ Mathworks",
     date: "May 2022 - July 2022",
     responsibilities: [
@@ -172,8 +172,8 @@ const expCards = [
   },
   {
     review: "Aman’s contributions to Full-stack web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: "/images/exp3.png",
-    logoPath: "/images/logo3.png",
+    imgPath: "./images/exp3.png",
+    logoPath: "./images/logo3.png",
     title: "Full Stack Developer",
     date: "June 2020 - December 2022",
     responsibilities: [
@@ -199,15 +199,15 @@ const expCards = [
 const expLogos = [
   {
     name: "logo1",
-    imgPath: "/images/logo1.png",
+    imgPath: "./images/logo1.png",
   },
   {
     name: "logo2",
-    imgPath: "/images/logo2.png",
+    imgPath: "./images/logo2.png",
   },
   {
     name: "logo3",
-    imgPath: "/images/logo3.png",
+    imgPath: "./images/logo3.png",
   },
 ];
 
@@ -217,64 +217,64 @@ const testimonials = [
     mentions: "@estherhoward",
     review:
       "I can’t say enough good things about Aman. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
-    imgPath: "/images/client1.png",
+    imgPath: "./images/client1.png",
   },
   {
     name: "Wade Warren",
     mentions: "@wadewarren",
     review:
       "Working with Aman was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
-    imgPath: "/images/client3.png",
+    imgPath: "./images/client3.png",
   },
   {
     name: "Guy Hawkins",
     mentions: "@guyhawkins",
     review:
       "Collaborating with Aman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aman is the ideal partner.",
-    imgPath: "/images/client2.png",
+    imgPath: "./images/client2.png",
   },
   {
     name: "Marvin McKinney",
     mentions: "@marvinmckinney",
     review:
       "Aman was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
-    imgPath: "/images/client5.png",
+    imgPath: "./images/client5.png",
   },
   {
     name: "Floyd Miles",
     mentions: "@floydmiles",
     review:
       "Aman’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
-    imgPath: "/images/client4.png",
+    imgPath: "./images/client4.png",
   },
   {
     name: "Albert Flores",
     mentions: "@albertflores",
     review:
       "Aman was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
-    imgPath: "/images/client6.png",
+    imgPath: "./images/client6.png",
   },
 ];
 
 const socialImgs = [
   {
     name: "linkedin",
-    imgPath: "/images/linkedin.png",
+    imgPath: "./images/linkedin.png",
     link: "https://www.linkedin.com/in/amanjeet-pani/",
   },
   {
     name: "insta",
-    imgPath: "/images/insta.png",
+    imgPath: "./images/insta.png",
     link: "https://www.instagram.com/aman.jeet_07/",
   },
   {
     name: "fb",
-    imgPath: "/images/fb.png",
+    imgPath: "./images/fb.png",
     link: "https://www.facebook.com/amanjeetpani/",
   },
   {
     name: "x",
-    imgPath: "/images/x.png",
+    imgPath: "./images/x.png",
     link: "https://twitter.com/amanjeetpani",
   },
 ];
